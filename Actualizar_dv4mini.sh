c968c826311828ae72aac4a5ad4b020ebe3a92a9
Manel <ea3eiz@ea3eiz.com>
2021-07-20 22:38:08 +0200

    1

--- Actualizar_dv4mini.sh
+++ Actualizar_dv4mini.sh
@@ -44,7 +44,7 @@ clear
                         sudo rm /usr/bin/dv_serial
                         sudo mkdir /home/pi/dv4mini
                         sudo chmod 777 -R /home/pi/dv4mini
-                        cd /home/pi/V107/DV4MINI/DV4MINI-RPI_2020_03_03
+                        cd /home/pi/V107/DV4MINI/2020_03_03
                         sudo cp dv_serial /home/pi/dv4mini
                         sudo cp dv4mini.exe /home/pi/dv4mini
                         sudo cp xref.ip /home/pi/dv4mini
