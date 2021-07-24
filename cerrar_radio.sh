270303b0aa74c156f158e9d454cd218842d8a495
Manel <ea3eiz@ea3eiz.com>
2021-07-24 20:58:45 +0200

    1

--- cerrar_radio.sh
+++ cerrar_radio.sh
@@ -4,7 +4,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
 
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=77x18 --title RADIO -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=88x19 --title RADIO -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
 sed -i "10c Name[es_ES]=Abrir RADIO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
--- ejecutar_radio.sh
+++ ejecutar_radio.sh
@@ -44,7 +44,7 @@ sleep 2
 sudo ./MMDVMHost MMDVM.ini
 cd /home/pi/Desktop
 sudo cp Abrir_Radio.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=77x18 --title=RADIO -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=88x19 --title=RADIO -e sudo sh ejecutar_radio.sh'" /home/pi/Abrir_Radio.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
 sed -i "10c Name[es_ES]=Abrir RADIO" /home/pi/Abrir_Radio.desktop
 sed -i "5c MMDVM=OFF" /home/pi/status.ini
